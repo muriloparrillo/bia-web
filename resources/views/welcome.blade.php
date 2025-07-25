a544a2852c7c7dd5e1198dbcca7cc19d5b86f445
muriloparrillo <muriloparrillo@gmail.com>
2025-07-25 16:29:43 -0300

    Adiciona texto de teste no Blade para validar Vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <title>BIA Web</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+<h1 style="color: red">Blade OK — Vue não carregou</h1>
 <body>
     <div id="app"></div>
 </body>
